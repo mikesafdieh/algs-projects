5b078668ba052a5e7f7030c00e4cdc4fd1836c62
Michael Safdieh <mikesafdieh@gmail.com>
2017-02-03 04:14:37 -0500

    Finished Deque implementation (no test client). Has not been tested.

--- 2 Deques and Randomized Queues/src/Deque.java
+++ 2 Deques and Randomized Queues/src/Deque.java
@@ -3,6 +3,8 @@
  */
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
+
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdRandom;
@@ -22,53 +24,100 @@ import edu.princeton.cs.algs4.StdRandom;
  **********************************************************************************************************************/
 
 public class Deque<Item> implements Iterable<Item> {
+    
+    private int n;            // number of elements in deque
+    private DoubleNode first; // pointer to first element of deque
+    private DoubleNode last;  // pointer to last element of deque
 
-    //TODO: Add necessary instance variables
+    // helper doubly-linked list class
+    private class DoubleNode {
+        Item item;       // data in the node
+        DoubleNode next; // link to next node
+        DoubleNode prev; // link to previous node
+    }
+
+    /**
+     * construct an empty deque
+     */
+    public Deque() {
+        n = 0;
+        first = null;
+        last = null;
+    }
 
     /**
      * is the deque empty?
      */
-    public boolean isEmpty() {
-        //TODO: Fill in...
-        return false;
-    }
+    public boolean isEmpty() { return first == null; }
 
     /**
      * return the number of items on the deque
      */
-    public int size() {
-        //TODO: Fill in...
-        return -1;
-    }
+    public int size() { return n; }
 
     /**
      * add the item to the front
      */
     public void addFirst(Item item) {
-        //TODO: Fill in...
+        if (item == null) throw new java.lang.NullPointerException("Can't add a null item");
+        DoubleNode oldFirst = first;
+        first = new DoubleNode();
+        first.item = item;
+        first.next = oldFirst;
+        first.prev = null;
+        if (oldFirst == null) // if we just added the first node (could have done "if (n == 0)")
+            last = first; // since this is the 1st node being added to the deque, we have to make sure last is updated
+        else
+            oldFirst.prev = first; // don't forget to handle the 2nd link!
+        n++;
     }
 
     /**
      * add the item to the end
      */
     public void addLast(Item item) {
-        //TODO: Fill in...
+        if (item == null) throw new java.lang.NullPointerException("Can't add a null item");
+        DoubleNode oldLast = last;
+        last = new DoubleNode();
+        last.item = item;
+        last.next = null;
+        last.prev = oldLast;
+        if (isEmpty())
+            first = last;
+        else
+            oldLast.next = last;
+        n++;
     }
 
     /**
      * remove and return the item from the front
      */
     public Item removeFirst() {
-        //TODO: Fill in...
-        return null;
+        if (isEmpty()) throw new NoSuchElementException("Queue underflow");
+        Item item = first.item; // store the item before deleting it
+        first = first.next;
+        if (isEmpty()) // NOTE: IntelliJ's warning/suggestion here is incorrect! This can indeed evaluate to true.
+            last = null; // avoid loitering
+        else
+            first.prev = null; // the new first node (originally the 2nd) has to make its prev pointer null
+            // NOTE: if the deque was empty, this operation would cause an error (that's why we need the else)!
+        n--;
+        return item;
     }
 
     /**
      * remove and return the item from the end
      */
     public Item removeLast() {
-        //TODO: Fill in...
-        return null;
+        if (isEmpty()) throw new NoSuchElementException("Queue underflow");
+        Item item = last.item; // store the item before deleting it
+        last = last.prev;
+        if (last == null)
+            first = null; // avoid loitering
+        else
+            last.next = null;
+        n--;
+        return item;
     }
 
     /**
@@ -78,20 +127,20 @@ public class Deque<Item> implements Iterable<Item> {
         return new DequeIterator();
     }
 
+    // an iterator, doesn't implement remove() since it's optional
     private class DequeIterator implements Iterator<Item> {
-        //TODO: Add necessary instance variables
+        private DoubleNode current = first;
 
-        public boolean hasNext() {
-            //TODO: Fill in...
-            return false;
-        }
+        public boolean hasNext() { return current != null; }
 
         public Item next() {
-            //TODO: Fill in...
-            return null;
+            if (!hasNext()) throw new NoSuchElementException();
+            Item item = current.item;
+            current = current.next;
+            return item;
         }
 
-        public void remove() {}
+        public void remove() { throw new UnsupportedOperationException(); }
     }
 
 
